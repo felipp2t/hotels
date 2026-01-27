c6f4e11dd7c17318c7a2feb0952de489cd68ddd3
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-01-26 22:41:11 -0300

    feat: implement User entity with properties and creation method

---
+++ src/domain/hospitality/enterprise/entities/user.ts
@@ -0,0 +1,43 @@
+import { Entity } from "@core/entities/entity";
+import type { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import type { Optional } from "@/core/types/optional";
+
+interface UserProps {
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class User extends Entity<UserProps> {
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  set password(newPassword: string) {
+    this.props.password = newPassword;
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(
+    props: Optional<UserProps, "createdAt" | "updatedAt">,
+    id?: UniqueEntityID
+  ) {
+    return new User(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+        updatedAt: props.updatedAt ?? new Date(),
+      },
+      id
+    );
+  }
+}
